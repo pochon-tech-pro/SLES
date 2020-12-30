dc19d14d8d285417cacb1a80de7f490227f0c827
pochon <pochon.tech.0519@gmail.com>
2020-12-30 23:28:05 +0900

    feat: ヘッダ部分作成

--- web/laravel/resources/views/order/index.blade.php
+++ web/laravel/resources/views/order/index.blade.php
@@ -1,15 +1,30 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>bootstrap sample</title>
+    <title>管理画面-注文</title>
     <link rel="stylesheet" href="./css/bootstrap.css">
     <script src="./js/jquery.js"></script>
     <script src="./js/bootstrap.js"></script>
   </head>
   <body>
+    <!-- header -->
+    <nav class="navbar navbar-default">
+      <!-- <div class="container-fluid"> -->
+      <div class="container">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Top</a>
+        </div>
+      </div>
+    </nav>
+    <!-- body -->
     <div class="container">
       <div class="panel panel-default">
-        <div class="panel-heading">フォーム1</div>
+        <div class="panel-heading">注文</div>
         <div class="panel-body">
           <form>
             <div class="form-group">
