f3b76709122a3ad432aa288d975f90231ff5d4f0
pochon <pochon.tech.0519@gmail.com>
2020-12-31 00:19:03 +0900

    feat: それらしい注文フォームの作成

--- web/laravel/resources/views/order/index.blade.php
+++ web/laravel/resources/views/order/index.blade.php
@@ -28,10 +28,10 @@
         <div class="panel-body">
           <form>
             <div class="form-group">
-              <label class="control-label">項目1</label>
-              <input class="form-control" type="text">
+              <label class="control-label">注文するユーザのID</label>
+              <input class="form-control" type="text" id="userId">
             </div>
-            <div class="form-group">
+            {{-- <div class="form-group">
               <label class="control-label">項目2</label>
               <div class="radio">
                 <label><input type="radio" name="radio">選択肢1</label>
@@ -46,21 +46,43 @@
                 <label><input type="checkbox" name="checkbox">選択肢2</label>
                 <label><input type="checkbox" name="checkbox">選択肢3</label>
               </div>
-            </div>
+            </div> --}}
             <div class="form-group">
-              <label class="control-label">項目4</label>
-              <select class="form-control">
-                <option>選択肢1</option>
-                <option>選択肢2</option>
-                <option>選択肢3</option>
-                <option>選択肢4</option>
-                <option>選択肢5</option>
+              <label class="control-label">注文ステータス</label>
+              <select class="form-control" id="status">
+                <option value="1">注文完了</option>
+                <option value="2">出荷完了</option>
+                <option value="3">その他</option>
               </select>
             </div>
-            <button class="btn btn-default">送信</button>
+            <div class="form-group">
+              <label class="control-label">商品選択</label>
+              <div class="checkbox" id="goods">
+                <label><input type="checkbox" name="checkbox" value="1001">歯ブラシ</label>
+                <label><input type="checkbox" name="checkbox" value="1002">歯磨き粉</label>
+              </div>
+            </div> 
+            <button class="btn btn-default" id="submitBtn">登録</button>
           </form>
         </div>
       </div>
     </div>
   </body>
+  <script>
+    // querySelector: https://www.webprofessional.jp/dom-manipulation-vanilla-javascript-no-jquery/
+    const $elUserId = document.querySelector('#userId');
+    const $elStatus = document.querySelector('#status');
+    // querySelectorAll & checkbox: https://techacademy.jp/magazine/29964
+    const $elGoods = document.querySelectorAll('#goods input[type="checkbox"]');
+    const $elSubmitBtn = document.querySelector('#submitBtn');
+    $elSubmitBtn.addEventListener('click', () => {
+      const goods = [];
+      for (let i = 0; i < $elGoods.length; i++){
+        if($elGoods[i].checked){ //(color1[i].checked === true)と同じ
+          goods.push($elGoods[i].value);
+        }
+      }
+      alert(`登録完了: ${$elUserId.value}, ${$elStatus.value}, ${goods.join(',')}`)
+    })
+  </script>
 </html>
