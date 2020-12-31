70e88b89473fc0c176daaec3e08cd7a7b767c9fa
pochon <pochon.tech.0519@gmail.com>
2021-01-01 01:32:13 +0900

    feat: 一覧・登録画面の雛形作成

--- web/laravel/app/Http/Controllers/OrderController.php
+++ web/laravel/app/Http/Controllers/OrderController.php
@@ -13,7 +13,7 @@ class OrderController extends Controller
      */
     public function index()
     {
-        //
+        return view('orders.index');
     }
 
     /**
@@ -23,7 +23,7 @@ class OrderController extends Controller
      */
     public function create()
     {
-        //
+        return view('orders.create');
     }
 
     /**
--- web/laravel/resources/views/orders/create.blade.php
+++ web/laravel/resources/views/orders/create.blade.php
@@ -1,26 +1,9 @@
-<html>
-  <head>
-    <meta charset="utf-8">
-    <title>管理画面-注文</title>
-    <link rel="stylesheet" href="./css/bootstrap.css">
-    <script src="./js/jquery.js"></script>
-    <script src="./js/bootstrap.js"></script>
-  </head>
+<!DOCTYPE html>
+<html lang="ja">
+@section('title', '注文')
+@include("parts.common")
   <body>
-    <!-- header -->
-    <nav class="navbar navbar-default">
-      <!-- <div class="container-fluid"> -->
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navigation">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Top</a>
-        </div>
-      </div>
-    </nav>
+    @include("parts.nav")
     <!-- body -->
     <div class="container">
       <div class="panel panel-default">
