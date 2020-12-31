9f97045f7f4eb56d8872b98912d688bfa2571ede
pochon <pochon.tech.0519@gmail.com>
2020-12-31 15:58:35 +0900

    feat: 管理者と会員のテーブル追加および注文テーブルも若干変更

--- web/laravel/database/migrations/2020_12_31_065212_create_orders_table.php
+++ web/laravel/database/migrations/2020_12_31_065212_create_orders_table.php
@@ -15,8 +15,10 @@ class CreateOrdersTable extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('user_id');
-            $table->foreign('user_id')->references('id')->on('users');
+            $table->unsignedBigInteger('member_id');
+            $table->foreign('member_id')->references('id')->on('members');
+            $table->unsignedBigInteger('manager_id');
+            $table->foreign('manager_id')->references('id')->on('managers');
             $table->string('status')->comment('注文ステータス');
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
