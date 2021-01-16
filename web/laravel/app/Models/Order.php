718c7ec036ed114e2bb00ace40b423b9de4721fd
pochon <pochon.tech.0519@gmail.com>
2021-01-16 15:16:37 +0900

    feat: TypeHintを定義

--- web/laravel/app/Models/Order.php
+++ web/laravel/app/Models/Order.php
@@ -4,7 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 
+/**
+ * @property int $id
+ * @property int $member_id
+ * @property int $manager_id
+ * @property string $status
+ * @property Carbon $created_at
+ * @property Carbon $update_at
+ * 
+ */
 class Order extends Model
 {
     use HasFactory;
